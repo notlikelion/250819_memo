a5c34bc3dc9c6ff173479aeca72aaf754a572adf
notlikelion <notlikelion@gmail.com>
2025-08-19 11:40:22 +0900

    ㅜㅜ

--- src/main/java/memo/DBTest.java
+++ src/main/java/memo/DBTest.java
@@ -9,10 +9,37 @@ public class DBTest {
     public static void main(String[] args) {
         UserDAO userDAO = new UserJdbcDAO();
         MemoDAO memoDAO = new MemoJdbcDAO();
+        // 사용자 만들기
+        System.out.println("😀 사용자 만들기 시작");
         try {
             userDAO.create("charlie", "Charlie Park");
+            // 3
         } catch (RuntimeException e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("😀 사용자 만들기 완료");
+        // 메모 작성
+        System.out.println("😀 메모 작성 시작");
+        Long memoId1 = null;
+        Long memoId2 = null; // scope
+        try {
+            memoId1 = memoDAO.create(3L, "안녕하세요", "찰리에요");
+            memoId2 = memoDAO.create(3L, "반갑습니다", "채플린에요");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("😀 메모 작성 완료");
+        // 단건 조회
+        System.out.println(
+                memoDAO.findById(memoId1)
+        );
+        // 유저 기준 목록 조회
+        System.out.println(
+                memoDAO.findByUserId(3L, 10, 0)
+        );
+        // 조인 조회
+        System.out.println(
+                memoDAO.findAllWithAuthor(10, 0)
+        );
     }
 }
