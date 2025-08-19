95ba58c013be8d00136a528cb793d0e9a0a279fa
notlikelion <notlikelion@gmail.com>
2025-08-19 13:18:14 +0900

    Servlet

--- src/main/java/memo/controller/MemoServlet.java
+++ src/main/java/memo/controller/MemoServlet.java
@@ -7,8 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import memo.model.dao.MemoDAO;
 import memo.model.dao.MemoJdbcDAO;
+import memo.model.dto.MemoDTO;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/memos", "/memos/new"})
 public class MemoServlet extends HttpServlet {
@@ -33,8 +35,30 @@ public class MemoServlet extends HttpServlet {
         String servletPath = req.getServletPath(); // 요청 주소
         if (servletPath.equals("/memos/new")) { // == 하면 안됩니다
             // 작성 (create)
+            // 파일 경로는 그대로...
+            req.getRequestDispatcher("/views/form.jsp").forward(req, resp);
             return;
         }
         // 목록
+        Long userId = 1L; // 1번
+        List<MemoDTO> memos = memoDAO.findByUserId(userId, 50, 0);
+        req.setAttribute("memos", memos);
+        req.getRequestDispatcher("/views/list.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8"); // 인코딩 처리 (폰트 안 깨지게)
+
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+
+        // 검증 생략...
+
+        Long userId = 1L;
+        memoDAO.create(userId, title, content);
+        // getContextPath -> [localhost:port/???]
+        resp.sendRedirect(req.getContextPath() + "/memos");
     }
 }
